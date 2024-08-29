27a4e1bea2b502943a597040042ddbf4644c63b2
Theo Reeves <theo_25@icloud.com>
2024-08-29 12:16:43 +0100

    more styling changes around colours

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,25 +13,32 @@ import LD from "@/components/light-dark";
 
 export default function Header() {
   return (
-    <nav className="sticky top-0 z-40 pt-2 self-center">
-      <Container className="pl-2 pr-2">
-        <Card>
+    <nav className="sticky top-0 z-40 pt-1 self-center">
+      <Container className="pl-2 pr-2 ">
+        <Card className="p-0 m-0">
           <Flex
             direction={"row"}
             justify={"between"}
             gap={"2"}
             align={"center"}
+            // className="pt-1"
           >
-            <div className="flex flex-row justify-center gap-2 items-center">
-              <Avatar
-                size={"2"}
-                radius="full"
-                src="/tbr-snap.jpg"
-                fallback="TR"
-              />
-              <Text size={"5"} color="orange">
-                <Strong>Theo Reeves</Strong>
-              </Text>
+            <div className="flex flex-row justify-center gap-2 items-center p-0 m-0">
+              <a href="/">
+                <Avatar
+                  size={"2"}
+                  radius="full"
+                  src="/tbr-snap.jpeg"
+                  fallback="TR"
+                />
+                <Text
+                  size={"5"}
+                  // color="orange"
+                  className="pl-1 text-orange-500 justify-center"
+                >
+                  <Strong>Theo Reeves</Strong>
+                </Text>
+              </a>
             </div>
             <div className="flex flex-row gap-2  items-center">
               <ActiveLink href={"/"}>
