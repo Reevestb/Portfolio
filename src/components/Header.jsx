bc2b701e75144fd61d7e3e3cecbde0b378ce24f0
Theo Reeves <theo_25@icloud.com>
2025-01-17 17:21:36 +0000

    changed header back to a card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,15 +15,15 @@ export default function Header() {
   return (
     <nav className="top-0 z-40 self-center max-w-4xl mx-auto pl-2 pr-2 pt-2 pb-2">
       {/* <Container className="pl-2 pr-2 pt-2 pb-2 "> */}
-      {/* <Card className="p-0 m-0"> */}
-      <Flex
-        direction={"row"}
-        justify={"between"}
-        gap={"2"}
-        align={"center"}
-        className="pl-2 pr-2 pt-2 pb-2 "
-      >
-        {/* <div className="flex flex-row justify-center gap-2 items-center p-0 m-0">
+      <Card className="p-0 m-0">
+        <Flex
+          direction={"row"}
+          justify={"between"}
+          gap={"2"}
+          align={"center"}
+          className="pl-2 pr-2 pt-2 pb-2 "
+        >
+          {/* <div className="flex flex-row justify-center gap-2 items-center p-0 m-0">
             <a href="/">
               <Avatar
                 size={"2"}
@@ -40,22 +40,22 @@ export default function Header() {
               </Text>
             </a>
           </div> */}
-        <div className="flex flex-row gap-2  items-center">
-          <ActiveLink href={"/"}>
-            <p className="text-sm">Home</p>
-          </ActiveLink>
-          <Separator orientation={"vertical"} size={"1"} />
-          <ActiveLink href={"/proj"}>
-            <p className="text-sm">Projects</p>
-          </ActiveLink>
-          {/* <Separator orientation={"vertical"} size={"1"} />
+          <div className="flex flex-row gap-2  items-center">
+            <ActiveLink href={"/"}>
+              <p className="text-sm">Home</p>
+            </ActiveLink>
+            <Separator orientation={"vertical"} size={"1"} />
+            <ActiveLink href={"/proj"}>
+              <p className="text-sm">Projects</p>
+            </ActiveLink>
+            {/* <Separator orientation={"vertical"} size={"1"} />
           <ActiveLink href={"/proj"}>
             <p className="text-sm">Proj</p>
           </ActiveLink> */}
-        </div>
-        <LD />
-      </Flex>
-      {/* </Card> */}
+          </div>
+          <LD />
+        </Flex>
+      </Card>
       {/* </Container> */}
     </nav>
   );
